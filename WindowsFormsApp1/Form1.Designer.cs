4bc22f4f633e249728cbb4f644abf278ced010ab
Pashkakakashka228 <pbelaev68@gmail.com>
2023-10-13 18:14:27 +0400

    Добавлены файлы дизайна Windows Forms

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.textBoxArray = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(214, 137);
+            this.buttonCalculate.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(101, 56);
+            this.buttonCalculate.TabIndex = 0;
+            this.buttonCalculate.Text = "Калькуляция";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // textBoxArray
+            // 
+            this.textBoxArray.Location = new System.Drawing.Point(28, 43);
+            this.textBoxArray.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxArray.Name = "textBoxArray";
+            this.textBoxArray.Size = new System.Drawing.Size(76, 20);
+            this.textBoxArray.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Массив чисел";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxArray);
+            this.Controls.Add(this.buttonCalculate);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.TextBox textBoxArray;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,9 +12,33 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        public string[] inputArray;
+        public string sumResult, productResult;
+
         public Form1()
         {
             InitializeComponent();
+        }
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            // считываем введенный пользователем массив чисел
+            string[] inputArray = textBoxArray.Text.Split(' ');
+
+            // забираем из класса calculation результаты
+            Calculation n1 = new Calculation();
+            Calculation n2 = new Calculation();
+
+            n1.GetSumResult(inputArray);
+            n2.GetSumResult(inputArray);
+
+
+            string s1 = n1.GetSumResult(inputArray);
+
+            string s2 = n2.GetProductResult(inputArray);
+
+            MessageBox.Show("Сумма положительных элементов: " + s1 + "; произведение чисел между минимальным и максимальным: " + s2);
+
+
         }
     }
 }
